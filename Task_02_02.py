0f2d54090de50841e1c96ff16ad9219c23c3c915
adarshs1002 <71135058+adarshs1002@users.noreply.github.com>
2021-06-10 13:51:53 +0530

    Positive numbers

---
+++ Task_02_02.py
@@ -0,0 +1,8 @@
+List1=[12,-7,5,64,-14]
+List2=[12,14,-95,3]
+for positive in List1:
+    if positive>0:
+        print(positive)
+for positive in List2:
+    if postive>0:
+        print(postive)        
